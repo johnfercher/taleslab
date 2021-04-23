8b1c625e8d0ac0541908ceaf277c3b240ab6662f
jfercher <johnathanfercher22@gmail.com>
2021-04-22 22:47:02 -0300

    keep

--- cmd/bytereader/main.go
+++ cmd/bytereader/main.go
@@ -10,40 +10,16 @@ import (
 
 func main() {
 	test := [][]byte{
-		{0, 0},
-		{64, 0},
-		{128, 0},
-		{192, 0},
-		{0, 1},
-		{64, 1},
-		{128, 1},
-		{192, 1},
-		{0, 2},
-		{64, 2},
-		{128, 2},
-		{192, 2},
-		{0, 3},
-		{64, 3},
-		{128, 3},
-		{192, 3},
-		{0, 4},
-		{64, 4},
-		{128, 4},
-		{192, 4},
-		{0, 5},
-		{64, 5},
-		{128, 5},
-		{192, 5},
-		{0, 6},
+		{0, 0, 200, 0},
+		{0, 0, 44, 1},
+		{0, 0, 144, 1},
 	}
-
-	fmt.Println(320.0 / 1536.0 * 360.0)
 
 	for _, value := range test {
 		reader := bytes.NewReader(value)
 		bufReader := bufio.NewReader(reader)
 
-		value, err := byteparser.BufferToInt16(bufReader)
+		value, err := byteparser.BufferToFloat32(bufReader)
 		if err != nil {
 			log.Fatalln(err)
 		}
