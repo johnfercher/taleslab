3e4cc663c5a825ce7befa03b0e34b9e7b731a0da
jfercher <johnathanfercher22@gmail.com>
2021-04-24 04:38:04 -0300

    Generating

--- pkg/slab/aggregator.go
+++ pkg/slab/aggregator.go
@@ -5,6 +5,8 @@ import (
 	"github.com/johnfercher/taleslab/pkg/slab/slabv2"
 )
 
+var MagicBytes = []byte{206, 250, 206, 209}
+
 type Aggregator struct {
 	SlabV1 *slabv1.Slab `json:"slab_v1,omitempty`
 	SlabV2 *slabv2.Slab `json:"slab_v2,omitempty`
